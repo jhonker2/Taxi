fd0e215ab622a127795d92108a3729925be264f7
Danny <dmacias6318@gmail.com>
2017-11-25 20:42:31 +0100

    25-11-2017
    insercion del mapa en carreras

--- resources/views/Carreras/mapa.blade.php
+++ resources/views/Carreras/mapa.blade.php
@@ -1,5 +1,31 @@
 @extends('layouts.App')
 
+@section('css')
+	<style type="text/css">
+		#map {
+        height: 100%;
+        width: 100%;
+       }
+
+	</style>
+@endsection
+
 @section('content')
-	<h1>Aqui va el map</h1>
+	     <div id="map"></div>
+         <script>
+        function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAiowIXlJ8xQr04iVGRB6lbqh_WcX7dYVQ&callback=initMap">
+    </script>
 @endsection
