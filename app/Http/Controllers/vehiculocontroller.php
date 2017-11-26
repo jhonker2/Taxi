e05661f84bc0204dc7a9b8072dd5e3ffaeb76681
Danny <dmacias6318@gmail.com>
2017-11-26 22:44:34 +0100

    26112017
    estaba manoceando tu codigo

--- app/Http/Controllers/vehiculocontroller.php
+++ app/Http/Controllers/vehiculocontroller.php
@@ -10,7 +10,7 @@ use DB;
 class vehiculocontroller extends Controller
 {
     //
-    public function index()
+    public function index()'¡'
     {
     	$chofer = DB::select("Select c.id, concat(p.nombres,' ',p.apellidos) as chofer from personas p,choferes c where p.id=c.id_persona");
         return view('vehiculos.formulario',compact('chofer'));
